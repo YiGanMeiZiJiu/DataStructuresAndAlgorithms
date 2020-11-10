7f0045d89b4a31419ca32f94d70c64ebee7e1a9a
新茶 <xincha.wct@raycloud.com>
2020-11-10 17:01:10 +0800

    project init

---
+++ src/main/java/com/code/datastructuresalgorithms/DataStructuresAlgorithmsApplication.java
@@ -0,0 +1,16 @@
+package com.code.datastructuresalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：王🍊
+ */
+@SpringBootApplication
+public class DataStructuresAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresAlgorithmsApplication.class, args);
+	}
+
+}
